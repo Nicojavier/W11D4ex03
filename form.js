54bab104b252e040605c214b8e7df8b5474a6426
Your Name <you@example.com>
2025-06-16 00:02:28 +0200

    Commit3

--- form.js
+++ form.js
@@ -1,11 +1,49 @@
 const formulario = document.querySelector("#Formulario1");
 
 formulario.addEventListener("submit", (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Previene el envío del formulario
 
-    const mensajeError = document.querySelectorAll(".mensajeError");
+    const mensajeError = document.querySelectorAll(".mensajeError"); // Limpia los mensajes de error
     mensajeError.forEach(mensajes.textContet = "");
 
     const nombre = document.querySelector("#nombre");
-    if (name.value.trim() === )
+    if (nombre.value.trim() === ''){
+        document.querySelector("#errorNombre").textContent = "El nombre es obligatorio.";
+        nombre.setAttribute("aria-invalid", "true");    // Indica que hay un error
+        mensajeError = true;
+    }else{
+        nombre.setAttribute("aria-invalid", "false"); // Indica que no hay error
+    }
+
+    const email = document.querySelector("#email");
+    if (email.value.trim() === ''){
+        document.querySelector("#errorEmail").textContent = "El email es obligatorio.";
+        email.setAttribute("aria-invalid", "true");    // Indica que hay un error
+        mensajeError = true;
+    }else{
+        email.setAttribute("aria-invalid", "false"); // Indica que no hay error
+    }
+
+    const asunto = document.querySelector("#asunto");
+    if (asunto.value.trim() === ''){
+        document.querySelector("#errorAsunto").textContent = "El asunto es obligatorio.";
+        asunto.setAttribute("aria-invalid", "true");    // Indica que hay un error
+        mensajeError = true;
+    }else{
+        asunto.setAttribute("aria-invalid", "false"); // Indica que no hay error
+    }
+
+    const mensaje = document.querySelector("#mensaje");
+    if (mensaje.value.trim() === ''){
+        document.querySelector("#errorMensaje").textContent = "El asunto es obligatorio.";
+        mensaje.setAttribute("aria-invalid", "true");    // Indica que hay un error
+        mensajeError = true;
+    }else{
+        asunto.setAttribute("aria-invalid", "false"); // Indica que no hay error
+    }
+
+    if (!mensajeError) { // Si no hay errores, se envia  el formulario
+        alert("Formulario enviado correctamente"); 
+        formulario.reset(); // Resetea el formulario
+    }
 })
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const aumentarFuente = document.querySelector("#aumentarFuente");
 const disminuirFuente = document.querySelector("#disminuirFuente")
+const contraste =document.querySelector("#contraste");
 const body = document.body;
 
 let TamFuente = 16;
 
 aumentarFuente.addEventListener("click", () => {
-    TamFuente = Math.min(264, TamFuente + 2);
+    TamFuente = Math.min(26, TamFuente + 2);
     body.style.fontSize = TamFuente + "px";
 });
 
@@ -13,3 +14,7 @@ disminuirFuente.addEventListener("click", () => {
     TamFuente = Math.max(10, TamFuente - 2);
     body.style.fontSize = TamFuente + "px";
 });
+
+contraste.addEventListener("click", () => {
+    
+} )
